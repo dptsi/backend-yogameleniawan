cc506c0b3fc34cd62ec00c48e8056788703a6fc0
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2023-05-06 20:37:04 +0700

    go variables

--- go-fundamental/03-variables.go
+++ go-fundamental/03-variables.go
@@ -20,13 +20,13 @@ func main() {
   fmt.Println(b)
   fmt.Println(c)
 
-//   Value Assignment After Declaration
+  //   Value Assignment After Declaration
 
   var student3 string
   student3 = "Yoga"
   fmt.Println(student3)
 
-//   There are some small differences between the var var :=:
+  //   There are some small differences between the var var :=:
 
 // var :
 // Can be used inside and outside of functions
@@ -36,6 +36,41 @@ func main() {
 // Can only be used inside functions
 // Variable declaration and value assignment cannot be done separately (must be done in the same line)
 
+  //  Multiple Variable Declaration
+  multipleVariable()
+  multipleVariableDifferentTypes()
+  variableBlock()
+}
 
+func multipleVariable() {
+  var a, b, c, d int = 1, 3, 5, 7
 
+  fmt.Println(a)
+  fmt.Println(b)
+  fmt.Println(c)
+  fmt.Println(d)
+}
+
+//   Multiple Variable Declaration with Different Types
+
+func multipleVariableDifferentTypes() {
+  var a, b = 6, "Hello"
+  c, d := 7, "World!"
+
+  fmt.Println(a)
+  fmt.Println(b)
+  fmt.Println(c)
+  fmt.Println(d)
+}
+
+func variableBlock() {
+	var (
+		a int
+		b int = 1
+		c string = "hello"
+	  )
+   
+	 fmt.Println(a)
+	 fmt.Println(b)
+	 fmt.Println(c)
 }
