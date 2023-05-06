4ae38695c9722f8636fdec9a4008525e4adfb12a
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2023-05-06 20:43:59 +0700

    go constant

---
+++ go-fundamental/04-constant.go
@@ -0,0 +1,28 @@
+package main
+import ("fmt")
+
+const PI = 3.14
+
+const (
+	A int = 1
+	B = 3.14
+	C = "Hi!"
+)
+
+func main() {
+  fmt.Println(PI)
+
+  const A = 1
+  A = 2 // Constants: Unchangeable and Read-only
+  // it doesn't work to assign/change value to constant
+  fmt.Println(A)
+
+  multipleConstDeclaration()
+}
+
+func multipleConstDeclaration() {
+  
+  fmt.Println(A)
+  fmt.Println(B)
+  fmt.Println(C)
+}
