d70e8720de8bb7d55e937e386b1fb64192d2f29b
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2023-05-08 09:58:49 +0700

    go slice

--- go-fundamental/08-slice.go
+++ go-fundamental/08-slice.go
@@ -12,4 +12,25 @@ func main() {
 	fmt.Printf("myslice = %v\n", myslice)
 	fmt.Printf("length = %d\n", len(myslice))
 	fmt.Printf("capacity = %d\n", cap(myslice))
+
+	// Change element of a slice
+
+	prices := []int{10, 20, 30}
+	prices[2] = 50
+	fmt.Println(prices[0])
+	fmt.Println(prices[2])
+
+	// Append Elements To a Slice
+
+	// slice_name = append(slice_name, element1, element2, ...)
+
+	myslice1 := []int{1, 2, 3, 4, 5, 6}
+	fmt.Printf("myslice1 = %v\n", myslice1)
+	fmt.Printf("length = %d\n", len(myslice1))
+	fmt.Printf("capacity = %d\n", cap(myslice1))
+
+	myslice1 = append(myslice1, 20, 21)
+	fmt.Printf("myslice1 = %v\n", myslice1)
+	fmt.Printf("length = %d\n", len(myslice1))
+	fmt.Printf("capacity = %d\n", cap(myslice1))
 }
