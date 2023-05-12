625471e98af694a8385af5fbf1d73271036613af
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2023-05-12 09:59:03 +0700

    major update dependency

--- 02-go-modules/go.mod
+++ 02-go-modules/go.mod
@@ -2,4 +2,4 @@ module github.com/dptsi/backend-yogameleniawan/02-go-modules
 
 go 1.20
 
-require github.com/yogameleniawan/go-module-hello v1.5.0
+require github.com/yogameleniawan/go-module-hello/v2 v2.1.0
--- 02-go-modules/module.go
+++ 02-go-modules/module.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	go_module_hello "github.com/yogameleniawan/go-module-hello"
+	go_module_hello "github.com/yogameleniawan/go-module-hello/v2"
 )
 
 func main() {
-	fmt.Println(go_module_hello.HelloModule())
+	fmt.Println(go_module_hello.HelloModule("Yoga Meleniawan Pamungkas"))
 }
