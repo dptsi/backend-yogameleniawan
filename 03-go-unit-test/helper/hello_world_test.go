8e936166753e2d64cfacc655cd7ea22b63afefac
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2023-05-12 10:47:25 +0700

    assertion test

--- 03-go-unit-test/helper/hello_world_test.go
+++ 03-go-unit-test/helper/hello_world_test.go
@@ -1,10 +1,29 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("Yoga Meleniawan Pamungkas")
 	if result != "Hello Yoga Meleniawan Pamungkas" {
 		t.Errorf("HelloWorld() = %s; want Hello Yoga Meleniawan Pamungkas", result)
 	}
+}
+
+func TestHelloWorldOther(t *testing.T) {
+	result := HelloWorld("Yoga Meleniawan Pamungkas")
+	if result != "Hello Yoga Meleniawan Pamungkas" {
+		t.Errorf("HelloWorld() = %s; want Hello Yoga Meleniawan Pamungkas", result)
+	}
+}
+
+func TestHelloWorldAssertion(t *testing.T) {
+	result := HelloWorld("Yoga Meleniawan Pamungkas")
+
+	assert.Equal(t, "Hello Yoga Meleniawan Pamungkas", result, "The two words should be the same.")
+	fmt.Println("TestHelloWorldAssertion() done")
 }
