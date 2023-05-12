0469b67fdd34436ef00a96413ebd71e32caae19f
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2023-05-12 11:04:29 +0700

    sub testing

--- 03-go-unit-test/helper/hello_world_test.go
+++ 03-go-unit-test/helper/hello_world_test.go
@@ -5,12 +5,25 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestMain(m *testing.M) {
 	fmt.Println("Start Testing Hello World")
 	m.Run()
 	fmt.Println("End Testing Hello World")
+}
+
+func TestSubTest(t *testing.T) {
+	t.Run("Yoga", func(t *testing.T) {
+		result := HelloWorld("Yoga Meleniawan Pamungkas")
+		require.Equal(t, "Hello Yoga Meleniawan Pamungkas", result, "The two words should be the same.")
+	})
+
+	t.Run("Meleniawan", func(t *testing.T) {
+		result := HelloWorld("Meleniawan")
+		require.Equal(t, "Hello Yoga Meleniawan Pamungkas", result, "The two words should be the same.")
+	})
 }
 
 func TestHelloWorld(t *testing.T) {
