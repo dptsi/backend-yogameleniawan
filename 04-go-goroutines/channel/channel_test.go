1c4319ffde01e6b6e7b95383ab1e0596b53109dd
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2023-05-15 11:15:09 +0700

    channel in out

--- 04-go-goroutines/channel/channel_test.go
+++ 04-go-goroutines/channel/channel_test.go
@@ -22,3 +22,23 @@ func TestCreateChannel(t *testing.T) {
 
 	time.Sleep(5 * time.Second)
 }
+
+func OnlyIn(channel chan<- string) {
+	time.Sleep(3 * time.Second)
+	channel <- "Hello World"
+}
+
+func OnlyOut(channel <-chan string) {
+	data := <-channel
+	fmt.Println(data)
+}
+
+func TestInOutChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+
+	time.Sleep(5 * time.Second)
+	close(channel)
+}
