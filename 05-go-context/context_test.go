8b68ec2ea46dc87bf33eaf532ee665a05c0781d4
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2023-05-16 13:57:41 +0700

    context with timeout

--- 05-go-context/context_test.go
+++ 05-go-context/context_test.go
@@ -69,6 +69,7 @@ func CreateCounter(ctx context.Context) chan int {
 			default:
 				desination <- counter
 				counter++
+				time.Sleep(1 * time.Second)
 			}
 		}
 	}()
@@ -94,6 +95,26 @@ func TestContextWithCancel(t *testing.T) {
 	}
 
 	cancel()
+
+	time.Sleep(2 * time.Second)
+
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+}
+
+func TestContextWithTimeout(t *testing.T) {
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+
+	parent := context.Background()
+	ctx, cancel := context.WithTimeout(parent, 5*time.Second)
+	defer cancel()
+
+	destination := CreateCounter(ctx)
+
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+
+	for n := range destination {
+		fmt.Println("Counter", n)
+	}
 
 	time.Sleep(2 * time.Second)
 
