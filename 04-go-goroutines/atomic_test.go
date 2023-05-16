bf9ceeb86058512e278ca739c21630607002ada7
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2023-05-16 09:53:15 +0700

    atomic

---
+++ 04-go-goroutines/atomic_test.go
@@ -0,0 +1,27 @@
+package go_routines
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"testing"
+)
+
+func TestAtomic(t *testing.T) {
+	var x int64 = 0
+	group := sync.WaitGroup{}
+
+	for i := 0; i < 1000; i++ {
+		go func() {
+			group.Add(1)
+			for j := 0; j < 100; j++ {
+				atomic.AddInt64(&x, 1)
+			}
+			group.Done()
+		}()
+	}
+
+	group.Wait()
+
+	fmt.Println("Counter = ", x)
+}
