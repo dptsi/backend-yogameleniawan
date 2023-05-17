781f59f75117582125fa5bfab69bda5fcd430325
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2023-05-17 08:39:35 +0700

    tipe data column

--- 06-go-database-mysql/sql_test.go
+++ 06-go-database-mysql/sql_test.go
@@ -2,8 +2,10 @@ package go_database_mysql
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestExecSql(t *testing.T) {
@@ -47,5 +49,50 @@ func TestQuerySql(t *testing.T) {
 
 		fmt.Println("Id", id)
 		fmt.Println("Name", name)
+	}
+}
+
+func TestQuerySqlComplex(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "SELECT id, name, email, balance, rating, birth_date, married, created_at FROM customer"
+	rows, error := db.QueryContext(ctx, script)
+
+	if error != nil {
+		panic(error)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var id, name string
+		var email sql.NullString
+		var balance int32
+		var rating float64
+		var createdAt time.Time
+		var birthDate sql.NullTime
+		var married bool
+		err := rows.Scan(&id, &name, &email, &balance, &rating, &birthDate, &married, &createdAt)
+
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("=======================")
+		fmt.Println("Id", id)
+		fmt.Println("Name", name)
+		if email.Valid {
+			fmt.Println("Email: ", email.String)
+		}
+		fmt.Println("Balance", balance)
+		fmt.Println("Rating", rating)
+		if birthDate.Valid {
+			fmt.Println("Birth Date", birthDate.Time)
+		}
+		fmt.Println("Married", married)
+		fmt.Println("Created At", createdAt)
+
 	}
 }
