a66d1935c216957cf653d26d1fa4ce316057dc32
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2023-05-17 11:23:38 +0700

    embed multiple files & path matcher

--- 07-go-embed/embed_test.go
+++ 07-go-embed/embed_test.go
@@ -1,6 +1,7 @@
 package go_embed
 
 import (
+	"embed"
 	_ "embed"
 	"fmt"
 	"io/fs"
@@ -25,4 +26,43 @@ func TestByte(t *testing.T) {
 		t.Error(err)
 	}
 
+}
+
+//go:embed files/a.txt
+//go:embed files/b.txt
+//go:embed files/c.txt
+var files embed.FS
+
+func TestFS(t *testing.T) {
+	a, err := files.ReadFile("files/a.txt")
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println(string(a))
+
+	b, err := files.ReadFile("files/b.txt")
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println(string(b))
+
+	c, err := files.ReadFile("files/c.txt")
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println(string(c))
+}
+
+//go:embed files/*.txt
+var path embed.FS
+
+func TestPathMatcher(t *testing.T) {
+	dirEntries, _ := path.ReadDir("files")
+	for _, entry := range dirEntries {
+		if !entry.IsDir() {
+			fmt.Println(entry.Name())
+			content, _ := path.ReadFile("files/" + entry.Name())
+			fmt.Println(string(content))
+		}
+	}
 }
