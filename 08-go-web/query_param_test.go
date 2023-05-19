3d96447886549ff1f89ac9ed1010238f2f8aa85c
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2023-05-19 08:40:36 +0700

    header

--- 08-go-web/query_param_test.go
+++ 08-go-web/query_param_test.go
@@ -63,3 +63,41 @@ func TestMultipleParameterValues(t *testing.T) {
 	body, _ := io.ReadAll(response.Body)
 	fmt.Println(string(body))
 }
+
+func RequestHeader(writer http.ResponseWriter, request *http.Request) {
+	contentType := request.Header.Get("content-type")
+	fmt.Fprint(writer, contentType)
+}
+
+func TestRequestHeader(t *testing.T) {
+	request := httptest.NewRequest("GET", "http://localhost:8080/hello", nil)
+	request.Header.Add("content-type", "application/json")
+	recorder := httptest.NewRecorder()
+
+	RequestHeader(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+}
+
+func ResponseHeader(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Add("x-location", "Surabaya")
+	writer.WriteHeader(http.StatusCreated)
+	fmt.Fprint(writer, "Success")
+}
+
+func TestResponseHeader(t *testing.T) {
+	request := httptest.NewRequest("GET", "http://localhost:8080/hello", nil)
+	recorder := httptest.NewRecorder()
+
+	ResponseHeader(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+	fmt.Println(response.Header.Get("x-location"))
+
+}
